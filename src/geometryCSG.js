c83c786fcbdf76dfc5e5f0f55e819d7514661a67
alan-wu <alan.wu@auckland.ac.nz>
2019-06-06 14:16:54 +1200

    Add in Promise polyfill for IE11 support.

--- src/geometryCSG.js
+++ src/geometryCSG.js
@@ -2,6 +2,7 @@ const THREE = require('three');
 const ThreeBSP = require('three-js-csg')(THREE);
 const Geometry = require('./geometry').Geometry;
 const work = require('webworkify-webpack');
+const Promise = require('promise-polyfill').default;
 //const work = undefined;
 const JSONLoader = require('./loader').JSONLoader;
 
