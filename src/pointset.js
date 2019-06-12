dd46ec771a60c334c654e9077c4901b92c918f31
alan-wu <alan.wu@auckland.ac.nz>
2019-06-12 14:44:36 +1200

    Add support for points.

---
+++ src/pointset.js
@@ -0,0 +1,84 @@
+const THREE = require('three');
+
+/**
+ * Provides an object which stores points and provides method which controls its position.
+ * This is created when a valid json file containing point is read into a {@link Zinc.Scene}
+ * object.
+ * 
+ * @class
+ * @author Alan Wu
+ * @return {Zinc.Geometry}
+ */
+exports.Pointset = function () {
+	this.morph = undefined;
+	let groupName = undefined;
+	this.timeEnabled = false;
+	this.morphColour = false;
+	this.isPointset = true;
+	
+	const getCircularTexture = () => {
+		var image = new Image();
+		image.src = require("./assets/disc.png");
+		const texture = new THREE.Texture();
+		texture.image = image;
+		texture.needsUpdate = true;
+		return texture;
+	}
+	
+	this.createMesh = (geometry, material) => {
+		if (geometry && material) {
+			const texture = getCircularTexture();
+			material.map = texture;
+			this.morph = new THREE.Points(geometry, material);
+			if (this.morph) {
+				this.morph.userData = this;
+				this.morph.name = groupName;
+			}
+		}
+		return this.morph;		
+	}
+	
+	this.setSize = size => {
+		if (this.morph && this.morph.material) {
+			this.morph.material.size = size;
+			material.needsUpdate = true;
+		}
+	}
+	
+	this.setSizeAttenuation = flag => {
+		if (this.morph && this.morph.material) {
+			this.morph.material.sizeAttenuation = flag;
+			material.needsUpdate = true;
+		}
+	}
+	
+	this.setName = groupNameIn => {
+		groupName = groupNameIn;
+		if (this.morph) {
+			this.morph.name = groupName;
+		}
+	}
+	
+	/**
+	 * Set the visibility of this pointset.
+	 * @param {Boolean} flag - visibility to be set for this pointset.
+	 */
+	this.setVisibility = flag => {
+		this.morph.visible = flag;
+	}
+	
+	/**
+	 * Get the bounding box for the whole set of glyphs.
+	 * 
+	 * @return {Three.Box3};
+	 */
+	this.getBoundingBox = () => {
+		if (this.morph.visible) {
+			this.morph.geometry.computeBoundingBox();
+			return this.morph.geometry.boundingBox;
+		}
+		return undefined;
+	}
+
+
+}
