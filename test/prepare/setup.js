18c04e1e3c76401a79c795bc7143297167376b1e
alan-wu <alan.wu@auckland.ac.nz>
2019-08-20 10:56:40 +1200

    Add support for requestAnimationFrame and cancelAnimationFrame for unit tests.

--- test/prepare/setup.js
+++ test/prepare/setup.js
@@ -1,6 +1,6 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const dom = new JSDOM(`<body><div id="container" styles="width:1024px;height:1024px"></div></body>`,{url: "http://localhost"});
+const dom = new JSDOM(`<body><div id="container" styles="width:1024px;height:1024px"></div></body>`,{url: "http://localhost",  pretendToBeVisual: true });
 global.window = dom.window;
 global.document = dom.window.document;
 global.Image = window.Image;
