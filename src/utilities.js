e12682dff091d3ed2f2be975db29a4d29cc34c4b
alan-wu <alan.wu@auckland.ac.nz>
2026-09-02 14:18:34 +1200

    Minor changes on label.

--- src/utilities.js
+++ src/utilities.js
@@ -732,10 +732,11 @@ function getCircularTexture() {
 
 function createNewSpriteText(text, height, colour, font, pixel, weight) {
   const sprite = new SpriteText(text, height, colour, font, pixel, weight);
+  sprite.canvasScale = 4;
   sprite.fontFace = font;
   sprite.fontSize = pixel;
   sprite.fontWeight = weight;
-  sprite.material.map.generateMipmaps = false;
+  sprite.material.map.generateMipmaps = true;
   sprite.material.map.anisotropy = 4;
   sprite.material.sizeAttenuation = false;
   sprite.material.alphaTest = 0.5;
